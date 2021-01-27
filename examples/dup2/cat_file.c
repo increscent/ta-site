2a048c002d4dcf6237ede5dfb5c3e4ad05d7adb2
Robert Williams <robert@increscent.org>
2021-01-27 15:05:35 -0600

    updated site

---
+++ examples/dup2/cat_file.c
@@ -0,0 +1,50 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * Usage: ./cat [filename]
+ * If filename is specified the program prints the contents of the file.
+ * Otherwise, it prints the contents of stdin.
+ */
+
+int main(int argc, char **argv)
+{
+    FILE *file;
+    char c;
+
+    if (argc > 1) {
+        // Open the file for reading
+        if ((file = fopen(argv[1], "r")) == NULL) {
+            fprintf(stderr, "Failed to open file for reading\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // Overwrite stdin with new file descriptor
+        if (dup2(fileno(file), fileno(stdin)) == -1) {
+            fprintf(stderr, "Failed to duplicate file descriptor onto stdin\n");
+            exit(EXIT_FAILURE);
+        }
+
+
+        if (argc > 2) {
+            if ((file = fopen(argv[2], "w")) == NULL) {
+                fprintf(stderr, "Failed to open file for writing\n");
+                exit(EXIT_FAILURE);
+            }
+
+            // Overwrite stdin with new file descriptor
+            if (dup2(fileno(file), fileno(stdout)) == -1) {
+                fprintf(stderr, "Failed to duplicate file descriptor onto stdout\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    // Read from stdin and write to stdout
+    while ((c = getchar()) != EOF)
+        putchar(c);
+
+    exit(EXIT_SUCCESS);
+}
